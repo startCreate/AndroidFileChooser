0638d631a4d39183c9602ee21be5a79174af8799
Vladimir <vov4ik11101@gmail.com>
2017-10-02 20:33:38 +0300

    impl ChoosePathListener callback

--- app/src/main/java/ir/sohreco/androidfilechooser/sample/MainActivity.java
+++ app/src/main/java/ir/sohreco/androidfilechooser/sample/MainActivity.java
@@ -32,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
                 @Override public void startLoading() {
 
                 }
-
                 @Override public void finishLoading() {
 
                 }
